2d86a4654437ea5ed469da0611c2aadd693827c5
Kuba <s17838@pjwstk.edu.pl>
2020-05-27 01:00:14 +0200

    Added withAuthentication.js and withAuthorization.js HOC for handling user login session and page access logic, added logout function to Logout molecule

--- src/molecules/Logout/Logout.js
+++ src/molecules/Logout/Logout.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import clsx from 'clsx';
 import Typography from '~/atoms/Typography';
+import { logoutUser } from '~/services/redux/actions';
 import useLogoutStyles from './styles';
 
 const Logout = ({ customClass }) => {
   const classes = useLogoutStyles();
-
+  const dispatch = useDispatch();
   return (
-    <div className={clsx(classes.container, customClass)}>
+    <div
+      onClick={() => dispatch(logoutUser())}
+      className={clsx(classes.container, customClass)}
+    >
       <Typography className={classes.name} variant={'h5'} component={'h5'}>
         {'Logout'}
       </Typography>
