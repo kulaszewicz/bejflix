d488a124af819dee4d0d0f5c74bf555fce5d219f
Kuba <s17838@pjwstk.edu.pl>
2020-06-07 03:18:24 +0200

    hotfix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const nextConfig = {
   experimental: { jsconfigPaths: true },
   publicRuntimeConfig: {
     API_ENDPOINT: process.env.API_ENDPOINT,
-    MOVIES_API_ENDPOINT: process.env.MOVIES_API_ENDPOINT,
+    MOVIES_API_ENDPOINT:
+      process.env.MOVIES_API_ENDPOINT ||
+      'https://us-central1-bejflix.cloudfunctions.net/api',
   },
 };
 
